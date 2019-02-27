e98eb8d6899d447a516320bb9312f8eee43b0250
HardCoreWare <bernardocodu@gmail.com>
2019-02-26 19:56:21 -0600

    ok

--- assets/js/accountrequest.js
+++ assets/js/accountrequest.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
                         
                         if(response==='success'){
 
-
+                            window.location.replace("http://www.w3schools.com");
 
                         }
 
