f209d06228bc1c11bf73a4bab19e9b8e66103168
HardCoreWare <bernardocodu@gmail.com>
2019-03-02 16:01:11 -0600

    corrigiendo modelos

--- assets/js/importtable.js
+++ assets/js/importtable.js
@@ -24,7 +24,51 @@ class ImportTable{
 
     readData(data){
 
-        
+        this.size=data.length;
+        this.headers=[];
+        this.matrix=[];
+
+        if(this.size){
+
+            let set=true;
+
+            data.forEach(row => {
+
+                if(set){
+
+                    set = false;
+
+                    this.headers.push("#");
+                    this.headers.push("Cuenta");
+                    this.headers.push("SuperConcepto");
+                    this.headers.push("Concepto");
+
+                    row.Montos.forEach(ammount=>{
+
+                        this.headers.push(ammount.Modulo);
+
+                    });
+
+                }
+
+                let line=[];
+
+                line.push({value:row.Id,key:"none"});
+                line.push({value:row.Cuenta,key:"none"});
+                line.push({value:row.Super_Concepto,key:"none"});
+                line.push({value:row.Concepto,key:"none"});
+
+                row.Montos.forEach(ammount => {
+
+                    line.push({value:ammount.Monto,key:row.Id+"-"+ammount.Modulo});
+
+                });
+
+                this.matrix.push(line);
+
+            });
+
+        }
 
     }
 
