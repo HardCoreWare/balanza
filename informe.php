922cdcb4afae94245d6353ca1ea2bb1f09bd3551
HardCoreWare <bernardocodu@gmail.com>
2019-03-03 17:15:11 -0600

    ok

--- informe.php
+++ informe.php
@@ -167,7 +167,6 @@
                                                             <option>1</option>
                                                             <option>2</option>
                                                             <option>3</option>
-                                                            <option>4</option>
 
                                                         </select>
 	                                                </div>
@@ -186,7 +185,6 @@
                                                             <option>1</option>
                                                             <option>2</option>
                                                             <option>3</option>
-                                                            <option>4</option>
 
                                                         </select>
 	                                                </div>
