a6756665a803570c0e37e4ccee2db00317b52f8e
HardCoreWare <bernardocodu@gmail.com>
2019-03-03 16:51:07 -0600

    ok

--- informe.php
+++ informe.php
@@ -143,32 +143,9 @@
 	                                            <div class="form-group">
 	                                                <label class="col-md-2 control-label">Text</label>
 	                                                <div class="col-md-10">
-	                                                    <input type="text" class="form-control" value="Some text value...">
-	                                                </div>
-	                                            </div>
-	                                            <div class="form-group">
-	                                                <label class="col-md-2 control-label" for="example-email">Email</label>
-	                                                <div class="col-md-10">
-	                                                    <input type="email" id="example-email" name="example-email" class="form-control" placeholder="Email">
-	                                                </div>
-	                                            </div>
-	                                            <div class="form-group">
-	                                                <label class="col-md-2 control-label">Password</label>
-	                                                <div class="col-md-10">
-	                                                    <input type="password" class="form-control" value="password">
-	                                                </div>
-	                                            </div>
-
-	                                            <div class="form-group">
-	                                                <label class="col-md-2 control-label">Placeholder</label>
-	                                                <div class="col-md-10">
-	                                                    <input type="text" class="form-control" placeholder="placeholder">
-	                                                </div>
-	                                            </div>
-	                                            <div class="form-group">
-	                                                <label class="col-md-2 control-label">Text area</label>
-	                                                <div class="col-md-10">
-	                                                    <textarea class="form-control" rows="5"></textarea>
+	                                                    <select type="text" class="form-control">
+                                                            
+                                                        </select>
 	                                                </div>
 	                                            </div>
 	                                        </form>
