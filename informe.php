7eba037d9f6440768e664fba7941795510902a65
HardCoreWare <bernardocodu@gmail.com>
2019-03-03 16:49:04 -0600

    ok

--- informe.php
+++ informe.php
@@ -138,7 +138,42 @@
 
                         			<div class="row">
 
-                                        <table id="datatable" class="table table-stripped"></table>
+                                        <div class="col-lg-6">
+                        					<form class="form-horizontal" role="form">
+	                                            <div class="form-group">
+	                                                <label class="col-md-2 control-label">Text</label>
+	                                                <div class="col-md-10">
+	                                                    <input type="text" class="form-control" value="Some text value...">
+	                                                </div>
+	                                            </div>
+	                                            <div class="form-group">
+	                                                <label class="col-md-2 control-label" for="example-email">Email</label>
+	                                                <div class="col-md-10">
+	                                                    <input type="email" id="example-email" name="example-email" class="form-control" placeholder="Email">
+	                                                </div>
+	                                            </div>
+	                                            <div class="form-group">
+	                                                <label class="col-md-2 control-label">Password</label>
+	                                                <div class="col-md-10">
+	                                                    <input type="password" class="form-control" value="password">
+	                                                </div>
+	                                            </div>
+
+	                                            <div class="form-group">
+	                                                <label class="col-md-2 control-label">Placeholder</label>
+	                                                <div class="col-md-10">
+	                                                    <input type="text" class="form-control" placeholder="placeholder">
+	                                                </div>
+	                                            </div>
+	                                            <div class="form-group">
+	                                                <label class="col-md-2 control-label">Text area</label>
+	                                                <div class="col-md-10">
+	                                                    <textarea class="form-control" rows="5"></textarea>
+	                                                </div>
+	                                            </div>
+	                                        </form>
+                                        </div>
+                                        <!-- end col -->
 
                                     </div>
                                     <!-- end row -->
