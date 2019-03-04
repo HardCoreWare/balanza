b64770d4c7b79b0846937c8ccac5260f35b353df
HardCoreWare <bernardocodu@gmail.com>
2019-03-03 19:18:40 -0600

    ok

--- assets/js/reporttable.js
+++ assets/js/reporttable.js
@@ -2,6 +2,21 @@ class Report{
 
     readData(data){
 
+        this.dataset={
+
+            pagado:{
+
+
+
+            },
+            noPagado:{
+
+                
+
+            }
+
+        };
+
         data.forEach(row => {
 
             if((row.Super_Concepto=="FACTOR HUMANO"||row.Super_Concepto=="FACTOR HUMANO PASIVOS")&&(row.Pagado=="1")){
