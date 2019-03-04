d1e3d0b08a86d41d6c687d16b8b8028eae4302f4
HardCoreWare <bernardocodu@gmail.com>
2019-03-03 21:09:14 -0600

    ok

--- assets/js/reporttable.js
+++ assets/js/reporttable.js
@@ -2,18 +2,34 @@ class ModularReport{
 
     constructor(){
 
-        this.
-
+        
+        
     }
 
     readData(data){
-
+    
         data.forEach(row => {
+            
+            if((row.Super_Concepto==="FACTOR HUMANO"||row.Super_Concepto==="FACTOR HUMANO PASIVOS")&&(row.Pagado==="1")){
 
-            
-            
+                
+
+            }
+
+            else if((row.Super_Concepto==="GASTOS GENERALES")&&(row.Pagado==="1")){
+
+                
+
+            }
+
+            else{
+
+
+
+            }
+
         });
-        
+
     }
 
 }
