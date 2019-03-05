ebfd1775abf7c42cc88f65b61bad6517779cf529
HardCoreWare <bernardocodu@gmail.com>
2019-03-05 11:50:56 -0600

    prueba de cecos front

---
+++ assets/js/cecosrequest.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $.ajax({
+
+        url:'http://35.243.156.112/plataforma/balanza/cecos/indice',
+        method:'GET',
+        success:function(response){
+
+            console.log(response);
+
+            
+
+        }
+
+    });
+
+
+});
