c9a4fa8e9e211929dd7f5fa0301bf8f050626342
HardCoreWare <bernardocodu@gmail.com>
2019-03-05 19:08:16 -0600

    preparando lecturas dinamicas cecos

--- assets/js/cecosrequest.js
+++ assets/js/cecosrequest.js
@@ -6,6 +6,22 @@ $(document).ready(function(){
 
         cecosTable.deleteTable("#datatable");
 
+        $.ajax({
+
+            url:'http://35.243.156.112/plataforma/balanza/cecos/indice',
+            method:'GET',
+            success:function(response){
+    
+                while(!response){}
+    
+                const data = JSON.parse(response);
+                cecosTable.readData(data);
+                cecosTable.writeTable("#datatable");
+    
+            }
+    
+        });
+
     });
 
 
