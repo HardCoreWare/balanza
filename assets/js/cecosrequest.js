b714a6a235646950d5c0938d5cace4fb6f1d390d
HardCoreWare <bernardocodu@gmail.com>
2019-03-05 20:00:39 -0600

    preparando lecturas dinamicas cecos

--- assets/js/cecosrequest.js
+++ assets/js/cecosrequest.js
@@ -2,6 +2,23 @@ $(document).ready(function(){
 
     cecosTable = new CecosTable();
 
+    $.ajax({
+
+        url:'http://35.243.156.112/plataforma/balanza/cecos/indice',
+        method:'GET',
+        success:function(response){
+
+            while(!response){}
+
+            const data = JSON.parse(response);
+            cecosTable.readData(data);
+            cecosTable.setOperation(0);
+            cecosTable.writeTable("#datatable");
+
+        }
+
+    });
+
     $("#btnRead").click(function(){
 
         cecosTable.deleteTable("#datatable");
@@ -26,22 +43,31 @@ $(document).ready(function(){
     });
 
 
-    $.ajax({
+    $("#btnRead").click(function(){
 
-        url:'http://35.243.156.112/plataforma/balanza/cecos/indice',
-        method:'GET',
-        success:function(response){
+        cecosTable.deleteTable("#datatable");
 
-            while(!response){}
+        $.ajax({
 
-            const data = JSON.parse(response);
-            cecosTable.readData(data);
-            cecosTable.setOperation(0);
-            cecosTable.writeTable("#datatable");
-
-        }
+            url:'http://35.243.156.112/plataforma/balanza/cecos/indice',
+            method:'GET',
+            success:function(response){
+    
+                while(!response){}
+    
+                const data = JSON.parse(response);
+                cecosTable.readData(data);
+                cecosTable.setOperation(1);
+                cecosTable.writeTable("#datatable");
+    
+            }
+    
+        });
 
     });
+
+
+
 
 
 });
