3a00be3ec8e19bba769da38f74e97bc5e27119d6
HardCoreWare <bernardocodu@gmail.com>
2019-03-06 17:22:27 -0600

    simplificando crud en cecos

--- assets/js/cecosreadform.js
+++ assets/js/cecosreadform.js
@@ -1,8 +1,21 @@
 static class CecosForm{
 
-    readSearchForm(,){
+    readSearchForm(moduleId,cecoId){
 
+        let module=$(moduleId).val();
+        let ceco=$(cecoId).val();
 
+        if(ceco===""){
+
+            ceco="all";
+            
+        }
+
+        else{
+
+            
+
+        }
 
     }
 
