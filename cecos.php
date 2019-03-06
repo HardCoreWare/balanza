1709f92cf3094b62104ef12c767d0760b2139326
HardCoreWare <bernardocodu@gmail.com>
2019-03-06 13:56:28 -0600

    preparando lecturas dinamicas cecos

--- cecos.php
+++ cecos.php
@@ -174,12 +174,14 @@
                             <div class="col-sm-12">
                                 <div class="card-box table-responsive">
                                     <div class="dropdown pull-right"></div>
+
+
                         			<h4 class="header-title m-t-0 m-b-30m dynamic-title">Centros de Costo</h4>
 
 
                                     <!-- comienza formilario de busqueda -->
                         			<div class="row">
-                                        <form class="form-horizontal" role="form">
+                                        <form class="form-horizontal" role="form" id="formSearch">
                                             <div class="col-lg-3">
                                             </div>
                                             <!-- end col -->
