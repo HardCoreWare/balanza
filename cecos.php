21ef16d6a0663d8010e2f5953452d4861afeff49
HardCoreWare <bernardocodu@gmail.com>
2019-03-06 17:07:40 -0600

    simplificando crud en cecos

--- cecos.php
+++ cecos.php
@@ -168,7 +168,12 @@
                                                     <label class="col-md-2 control-label">Empresa</label>
                                                     <div class="col-md-10">
                                                         <select type="text" class="form-control">
-                                                            <option value="TODOS">TODOS</option>
+                                                        <option value="all">TODOS</option>
+                                                        <option value="BANCO">BANCO</option>
+                                                        <option value="CASA">CASA</option>
+                                                        <option value="GRUPO">GRUPO</option>
+                                                        <option value="OPERADORA">OPERADORA</option>
+                                                        <option value="SERVICIOS">SERVICIOS</option>
                                                         </select>
                                                     </div>
                                                 </div>
